ec8ce1151f19e4871ad9cc99905b55911a796bdc
rghosh <rghosh.ds@gmail.com>
2024-12-19 21:30:25 +0530

    add db models

---
+++ library_management_app/models.py
@@ -0,0 +1,13 @@
+from . import db
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    available = db.Column(db.Boolean, default=True)
+
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
