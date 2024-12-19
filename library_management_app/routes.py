f28480a93e87a92cccf3a8ea038d305bfaf5e1eb
rghosh <rghosh.ds@gmail.com>
2024-12-19 22:41:12 +0530

    add pagination

--- library_management_app/routes.py
+++ library_management_app/routes.py
@@ -2,14 +2,23 @@ from flask import Blueprint, jsonify, request
 from library_management_app.extensions import db
 from library_management_app.models import Book
 
-
 main = Blueprint('main', __name__)
 
 
 @main.route('/books', methods=['GET'])
 def get_books():
-    books = Book.query.all()
-    return jsonify([{"id": book.id, "title": book.title, "author": book.author} for book in books])
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+
+    pagination = Book.query.paginate(page=page, per_page=per_page)
+    books = pagination.items
+
+    return jsonify({
+        "books": [{"id": book.id, "title": book.title, "author": book.author} for book in books],
+        "total": pagination.total,
+        "pages": pagination.pages,
+        "current_page": pagination.page
+    })
 
 
 @main.route('/books', methods=['POST'])
